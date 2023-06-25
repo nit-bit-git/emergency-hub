4cab131c8edd2a228156db6628bd53758c0392a3
Nitish G. Saji <nitishsaji2001@gmail.com>
2023-06-25 09:39:08 +0530

    update

--- rise/src/components/SideBar.jsx
+++ rise/src/components/SideBar.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {
+  FiSettings,
+  FiUsers,
+  FiAlertOctagon,
+  FiMenu
+} from "react-icons/fi";
+const handleItemClick = (item) => {
+  setIsOpen(!isOpen);
+};
 
 class SideNavbar extends React.Component {
   render() {
     return (
       <div className="side-navbar">
+        
         <ul>
-          <li>Newsfeed</li>
-          <li>Report</li>
-          <li>Volunteering</li>
+          <div>
+          <li onClick={() => handleItemClick('MenuItem1')}>Newsfeed</li>
+          </div>
+          <li onClick={() => handleItemClick('MenuItem1')}>Report</li>
+          <li onClick={() => handleItemClick('MenuItem1')}>Volunteering</li>
         </ul>
       </div>
     );
